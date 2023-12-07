601c7fad6eb523b9c2cfeb7dab6441f5ea718b4e
Saianjan <saianjan1234@gmail.com>
2023-12-07 14:38:56 +0530

    Welcome To First Commit

--- StudentDatabase.js
+++ StudentDatabase.js
@@ -1,1 +1,142 @@
 console.log("Welcome To Student DataBase!");
+let studentDetails = [{
+    roll_no: 101,
+    name: "Sophia Smith",
+    class: 6,
+    gender: "Female",
+    test_score: []
+},
+{
+    roll_no: 102,
+    name: "Ethan Johnson",
+    class: 6,
+    gender: "Male",
+    test_score: []
+},
+{
+    roll_no: 103,
+    name: "Ava Williams",
+    class: 6,
+    gender: "Female",
+    test_score: []
+},
+{
+    roll_no: 104,
+    name: "Noah Brown",
+    class: 6,
+    gender: "Male",
+    test_score: []
+},
+{
+    roll_no: 105,
+    name: "Olivia Jones",
+    class: 6,
+    gender: "Female",
+    test_score: []
+},
+{
+    roll_no: 101,
+    name: "Liam Davis",
+    class: 7,
+    gender: "Male",
+    test_score: []
+},
+{
+    roll_no: 102,
+    name: "Emma Martinez",
+    class: 7,
+    gender: "Female",
+    test_score: []
+},
+{
+    roll_no: 103,
+    name: "Mia Wilson",
+    class: 7,
+    gender: "Female",
+    test_score: []
+},
+{
+    roll_no: 104,
+    name: "Lucas Taylor",
+    class: 7,
+    gender: "Male",
+    test_score: []
+},
+{
+    roll_no: 105,
+    name: "Aiden Anderson",
+    class: 7,
+    gender: "Male",
+    test_score: []
+},
+{
+    roll_no: 101,
+    name: "Isabella Thomas",
+    class: 8,
+    gender: "Female",
+    test_score: []
+},
+{
+    roll_no: 102,
+    name: "James White",
+    class: 8,
+    gender: "Male",
+    test_score: []
+},
+{
+    roll_no: 103,
+    name: "Avery Clark",
+    class: 8,
+    gender: "Female",
+    test_score: []
+},
+{
+    roll_no: 104,
+    name: "Mason Turner",
+    class: 8,
+    gender: "Male",
+    test_score: []
+},
+{
+    roll_no: 105,
+    name: "Charlotte Harris",
+    class: 8,
+    gender: "Female",
+    test_score: []
+},
+{
+    roll_no: 101,
+    name: "Evelyn Scott",
+    class: 9,
+    gender: "Female",
+    test_score: []
+},
+{
+    roll_no: 102,
+    name: "Logan King",
+    class: 9,
+    gender: "Male",
+    test_score: []
+},
+{
+    roll_no: 103,
+    name: "Harper Turner",
+    class: 9,
+    gender: "Female",
+    test_score: []
+},
+{
+    roll_no: 104,
+    name: "Jackson Lee",
+    class: 9,
+    gender: "Male",
+    test_score: []
+},
+{
+    roll_no: 105,
+    name: "Abigail Baker",
+    class: 9,
+    gender: "Female",
+    test_score: []
+}
+]
